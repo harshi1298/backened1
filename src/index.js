2f166ded106634c90c24d3098c8ff16a3494f51d
harshi1298 <11082004harshit@gmail.com>
2024-03-11 18:32:24 +0530

    add project file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 /*"scripts": {
+   // install package.json file by npm init
+   //install npm prettier because not collapse by the use of npm i -D prettier
+    first install the nodemon by npm i -D nodemon
     "dev": "nodeman src/index.js"//"it reload all files in index.js"
   },*/
